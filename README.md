abab40f67c7cd59f3ff60cae1cc7abba808863eb
prydin <prydin@vmware.com>
2021-01-20 12:03:14 -0500

    Added config through args. Added forever mode

--- README.md
+++ README.md
@@ -4,18 +4,27 @@ This simple script checks the status of NSX-V edges and reports any redeployment
 
 ## Usage
 ```bash
-python nsxcheck.py [path to config file]
+usage: nsxcheck.py [-h] [--config CONFIG] [--nsxurl NSXURL] [--nsxuser NSXUSER]
+               [--nsxpassword NSXPASSWORD] [--vropsurl VROPSURL]
+               [--vropsuser VROPSUSER] [--vropspassword VROPSPASSWORD]
+               [--forever FOREVER]
+
 ```
+
+Configuration can be done either through a config file or directly through parameters. The ```--forever``` flag
+lets the collector stay running indefintely collecting every 60 seconds. Without the flag, the collector run once and
+saves the state in a temporary file. The latter mode is ideal for scheduling the script using e.g. cron or
+vRealize Orchestrator.
 
 ### Configuration file format
 ```yaml
 vrops:
   url: https://vrops.corp.local
-  username: admin
+  user: admin
   password: secret
 
 nsx:
   url: https://nsx.corp.local
-  username: admin
+  user: admin
   password: secret
 ```
--- main.py
+++ main.py
@@ -3,71 +3,134 @@ from vropsclient import VRopsClient
 import time
 import yaml
 import sys
+import argparse
+import tempfile
+import os
+import json
 
-if len(sys.argv) != 2:
-    print("Usage: nsxcheck [config file]")
-    sys.exit(1)
+parser = argparse.ArgumentParser(description='NSXcheck');
+parser.add_argument('--config', help='Config file')
+parser.add_argument('--nsxurl', help='The NSX manager URL')
+parser.add_argument('--nsxuser', type=str, help='The NSX manager user')
+parser.add_argument('--nsxpassword', help='The NSX manager password')
+parser.add_argument('--vropsurl', help='The NSX manager URL')
+parser.add_argument('--vropsuser', type=str, help='The NSX manager user')
+parser.add_argument('--vropspassword', help='The NSX manager password')
+parser.add_argument('--forever', help='Run forever', type=bool, default=False)
 
-with open(sys.argv[1]) as file:
-    config = yaml.full_load(file)
+args = parser.parse_args()
 
-nsxConf = config['nsx']
-vropsConf = config['vrops']
+nsxUrl = ''
+nsxUser = ''
+nsxPassword = ''
+vropsUrl = ''
+vropsUser = ''
+vropsPassword = ''
 
-nsx = nsxclient.NSXClient(nsxConf['url'], nsxConf['username'], nsxConf['password'])
-vrops = VRopsClient(vropsConf['url'], vropsConf['username'], vropsConf['password'])
+# Config using file?
+if args.config:
+    with open(args.config) as file:
+        config = yaml.full_load(file)
+
+    nsxConf = config['nsx']
+    vropsConf = config['vrops']
+    nsxUrl = nsxConf['url']
+    nsxUser = nsxConf['user']
+    nsxPassword = nsxConf['password']
+
+    vropsUrl = vropsConf['url']
+    vropsUser = vropsConf['user']
+    vropsPassword = vropsConf['password']
+else:
+    nsxUrl = args.nsxurl
+    nsxUser = args.nsxuser
+    nsxPassword = args.nsxpassword
+    vropsUrl = args.vropspassword
+    vropsUser = args.vropsuser
+    vropsPassword = args.vropspassword
+
+# Did we get enough config?
+if not (nsxUrl and nsxUser and nsxPassword and vropsUrl and vropsUser and vropsPassword):
+    print("URL, user and password must be specified for NSX and vR Ops")
+    parser.print_usage()
+    exit(1)
+
+
+
+nsx = nsxclient.NSXClient(nsxUrl, nsxUser, nsxPassword)
+vrops = VRopsClient(vropsUrl, vropsUser, vropsPassword)
 
 def send_event(message, edge):
     resource_id = vrops.resource_id_by_name(edge['name'])
     vrops.push_event(resource_id, message, 'NOTIFICATION')
     print('%s - %s' % (message, edge['name']))
 
-def run():
+def run_forever():
     edge_table = {}
     period = 60 # Sample every 60 seconds
 
     while True:
         t0 = time.time()
-        remaining = set(edge_table.keys())
-        edges = nsx.get_edges()
-        for edge in edges['edgePage']['data']:
-            key = edge['name']
-            if key in edge_table:
-                previous = edge_table[key]
-
-                # Object id changed?
-                if edge['objectId'] != previous['objectId']:
-                    send_event("Edge redeployed", edge)
-
-                # Size changed?
-                oldSize = previous['appliancesSummary']['applianceSize']
-                newSize = edge['appliancesSummary']['applianceSize']
-                if oldSize != newSize:
-                    send_event("Edge resized from %s to %s" % (oldSize, newSize), edge)
-
-                # We've dealt with this, so remove it from the set of potentially dangling edges
-                remaining.remove(key)
-            edge_table[key] = edge
-
-        # Check if we had more edges in our previous run (i.e. edges disappeared on us)
-        for key in remaining:
-            send_event("Edge removed", edge_table[key])
-            edge_table.pop(key)
+        collect(edge_table)
 
         # Wait whatever time is left of the period
         delay = period - (time.time() - t0)
         if delay > 0:
             time.sleep(delay)
 
-while True:
-    try:
-        run()
-    except KeyboardInterrupt:
-        print("Aborted by operator")
-        sys.exit(0)
-    except Exception as e:
-        print("Error occurred. Trying again in 60 seconds. Error: % s" % str(e))
-        time.sleep(60)
+
+def collect(edge_table):
+    remaining = set(edge_table.keys())
+    edges = nsx.get_edges()
+    for edge in edges['edgePage']['data']:
+        key = edge['name']
+        if key in edge_table:
+            previous = edge_table[key]
+
+            # Object id changed?
+            if edge['objectId'] != previous['objectId']:
+                send_event("Edge redeployed", edge)
+
+            # Size changed?
+            oldSize = previous['appliancesSummary']['applianceSize']
+            newSize = edge['appliancesSummary']['applianceSize']
+            if oldSize != newSize:
+                send_event("Edge resized from %s to %s" % (oldSize, newSize), edge)
+
+            # We've dealt with this, so remove it from the set of potentially dangling edges
+            remaining.remove(key)
+        edge_table[key] = edge
+
+    # Check if we had more edges in our previous run (i.e. edges disappeared on us)
+    for key in remaining:
+        send_event("Edge removed", edge_table[key])
+        edge_table.pop(key)
+
+
+def run_once():
+    dir = tempfile.gettempdir()
+    p = os.path.join(dir, 'nsxstate.json')
+    if os.path.exists(p):
+        with open(p) as file:
+            edge_table = json.load(file)
+    else:
+        edge_table = {}
+    collect(edge_table)
+    with open(p, "w") as file:
+        json.dump(edge_table, file)
+
+if args.forever:
+    while True:
+        try:
+            run_forever()
+        except KeyboardInterrupt:
+            print("Aborted by operator")
+            sys.exit(0)
+        except Exception as e:
+            print("Error occurred. Trying again in 60 seconds. Error: % s" % str(e))
+            time.sleep(60)
+else:
+    run_once()
 
 
 
